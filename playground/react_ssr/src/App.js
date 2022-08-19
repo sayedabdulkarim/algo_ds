cf10069810919048853d07f79f4f4ab10b18a7d7
Abdul8008 <sayed@capitalnumbers@gmail.com>
2022-08-20 02:56:10 +0530

    updated

--- playground/react_ssr/src/App.js
+++ playground/react_ssr/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  //state
+  const [ count, setCount ] = useState(0);
+  // func
+  const increment = () => {
+    setCount( prev => prev + 1 )
+  } 
+  const decrement = () => {
+    setCount( prev => prev - 1 )
+  } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <p style={{
+        color: "red"
+      }}>Count - { count}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
